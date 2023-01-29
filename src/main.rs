86b347be41a78f79387e6cdf0059b065f6e21181
Pavitra Golchha <hello@pavi2410.me>
2023-01-30 00:24:35 +0530

    Renamed max_expansion_length to limit

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[arg(short, long)]
-    length: usize, // todo: min & max lengths of generated string
+    limit: usize,
     pattern: String, // support regex as well as globs
 }
 
@@ -12,7 +12,7 @@ fn main() {
     let args = Cli::parse();
     println!("\x1b[34m{:?}\x1b[0m", args);
 
-    match generate_from_glob(args.pattern, args.length) {
+    match generate_from_glob(args.pattern, args.limit) {
         Ok(s) => println!("{}", s),
         Err(e) => println!("\x1b[31m{}\x1b[0m", e),
     }
@@ -22,17 +22,17 @@ fn main() {
 // todo: generate multiple strings
 // todo: support optional wildcards in glob
 // todo: transform glob to regex (refer wikipedia on glob)
-fn generate_from_glob(regex: String, max_expansion_length: usize) -> Result<String, &'static str> {
+fn generate_from_glob(regex: String, limit: usize) -> Result<String, &'static str> {
     let mut gen_str: String = String::new();
     let mut last_char: Option<char> = None;
-    let mut length_left = max_expansion_length;
+    let mut length_left = limit;
 
     for ch in regex.chars() {
         match ch {
             'A'..='Z' | 'a'..='z' | '0'..='9' => {
                 gen_str.push(ch);
                 last_char = Some(ch);
-                length_left = max_expansion_length;
+                length_left = limit;
             }
             '*' => {
                 if last_char.is_none() {
